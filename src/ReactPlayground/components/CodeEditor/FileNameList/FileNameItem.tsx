b13b7396217b10ae43690568deb254233a32acc4
likeztmy <413713942@qq.com>
2024-08-05 21:18:19 +0800

    feat: 新增文件增删改

--- src/ReactPlayground/components/CodeEditor/FileNameList/FileNameItem.tsx
+++ src/ReactPlayground/components/CodeEditor/FileNameList/FileNameItem.tsx
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import React, { MouseEventHandler, useEffect, useRef, useState } from "react";
 import styles from "./index.module.scss";
 import classnames from "classnames";
+import { Popconfirm } from "antd";
 
 export interface FileNameItemProps {
   value: string;
   actived: boolean;
+  creating: boolean;
+  readonly: boolean;
   onClick: () => void;
+  onRemove: () => void;
+  onEditComplete: (value: string) => void;
 }
 
 export const FileNameItem: React.FC<FileNameItemProps> = (props) => {
-  const { value, actived, onClick } = props;
+  const {
+    value,
+    actived,
+    creating,
+    readonly,
+    onClick,
+    onRemove,
+    onEditComplete,
+  } = props;
 
   const [name, setName] = useState(value);
+  const [isEdit, setIsEdit] = useState(creating);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleDoubleClick = () => {
+    setIsEdit(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const handleBlur = () => {
+    setIsEdit(false);
+    onEditComplete(name);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  }, [creating]);
 
   return (
     <div
@@ -21,7 +54,40 @@ export const FileNameItem: React.FC<FileNameItemProps> = (props) => {
       )}
       onClick={onClick}
     >
-      <span>{name}</span>
+      {isEdit ? (
+        <input
+          ref={inputRef}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onBlur={handleBlur}
+          className={styles["tab-item-input"]}
+        />
+      ) : (
+        <>
+          <span onDoubleClick={!readonly ? handleDoubleClick : () => {}}>
+            {name}
+          </span>
+          {!readonly && (
+            <Popconfirm
+              title="确认删除该文件吗？"
+              onConfirm={(e) => {
+                e?.stopPropagation();
+                onRemove();
+              }}
+              okText="确认"
+              cancelText="取消"
+              overlayClassName="custom-popconfirm"
+            >
+              <span style={{ marginLeft: 5, display: "flex" }}>
+                <svg width="12" height="12" viewBox="0 0 24 24">
+                  <line stroke="#999" x1="18" y1="6" x2="6" y2="18"></line>
+                  <line stroke="#999" x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </span>
+            </Popconfirm>
+          )}
+        </>
+      )}
     </div>
   );
 };
--- src/ReactPlayground/components/CodeEditor/FileNameList/index.tsx
+++ src/ReactPlayground/components/CodeEditor/FileNameList/index.tsx
@@ -1,7 +1,13 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { PlaygroundContext } from "../../../PlaygroundContext";
 import { FileNameItem } from "./FileNameItem";
 import styles from "./index.module.scss";
+
+import {
+  APP_COMPONENT_FILE_NAME,
+  ENTRY_FILE_NAME,
+  IMPORT_MAP_FILE_NAME,
+} from "../../../files";
 
 export default function FileNameList() {
   const {
@@ -14,23 +20,78 @@ export default function FileNameList() {
   } = useContext(PlaygroundContext);
 
   const [tabs, setTabs] = useState([""]);
+  const [creating, setCreating] = useState(false);
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleWheel = (e: WheelEvent) => {
+    e.preventDefault();
+    if (!scrollContainerRef.current) return;
+    scrollContainerRef.current.scrollLeft += e.deltaY;
+  };
 
   useEffect(() => {
     setTabs(Object.keys(files));
   }, [files]);
 
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer) {
+      scrollContainer.addEventListener("wheel", handleWheel, {
+        passive: false,
+      });
+    }
+
+    return () => {
+      if (scrollContainer) {
+        scrollContainer.removeEventListener("wheel", handleWheel);
+      }
+    };
+  }, []);
+
+  const handleEditComplete = (oldName: string, newName: string) => {
+    if (newName === oldName) return;
+    updateFileName(oldName, newName);
+    setSelectedFileName(newName);
+    setCreating(false);
+  };
+
+  const addTab = () => {
+    setCreating(true);
+    addFile("Comp" + Math.random().toString().slice(2, 8) + ".tsx");
+  };
+
+  const handleRemove = (fileName: string) => {
+    removeFile(fileName);
+    setSelectedFileName(ENTRY_FILE_NAME);
+  };
+
+  const readonlyFileNames = [
+    ENTRY_FILE_NAME,
+    IMPORT_MAP_FILE_NAME,
+    APP_COMPONENT_FILE_NAME,
+  ];
+
   return (
-    <div className={styles.tabs}>
-      {tabs.map((tab, index) => (
+    <div className={styles.tabs} ref={scrollContainerRef}>
+      {tabs.map((tab, index, arr) => (
         <FileNameItem
           key={tab + index}
           value={tab}
           actived={selectedFileName === tab}
+          readonly={readonlyFileNames.includes(tab)}
+          creating={creating && index === arr.length - 1}
           onClick={() => {
             setSelectedFileName(tab);
           }}
+          onRemove={() => {
+            handleRemove(tab);
+          }}
+          onEditComplete={(newName) => handleEditComplete(tab, newName)}
         />
       ))}
+      <div className={styles.add} onClick={addTab}>
+        +
+      </div>
     </div>
   );
 }
